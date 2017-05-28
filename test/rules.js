641928ebd1af297b75b5a7f112a287a39e40fc5e
Todd Wolfson <todd@twolfson.com>
2017-05-27 22:07:33 -0700

    Completed error formatting

--- test/rules.js
+++ test/rules.js
@@ -14,22 +14,32 @@ var testUtils = {
       // Define our CLI engine
       // http://eslint.org/docs/developer-guide/nodejs-api#executeonfiles
       var cli = new eslint.CLIEngine({
+        envs: ['node'],
         configFile: eslintConfigFilepath,
         useEslintrc: false
       });
 
       // Lint our file and save the results
+      // report = {
+      //    results: [{filePath, messages: [messageObj, ...], errorCount: 1, warningCount: 0}],
+      //    errorCount: 1, warningCount: 0}
+      // messageObj = {
+      //    ruleId: 'no-undef', severity: 2, message: '... is not defined',
+      //    line, column, nodeType: 'Identifier', source: 'exports.exports = ...'}
       var report = cli.executeOnFiles([filepath]);
-      this.report = report;
+      this.result = report.results[0];
+      this.messageStr = this.result.messages.map(function extractMessageStr (messageObj) {
+        return messageObj.message;
+      }).join('\n');
     });
     after(function cleanup () {
-      delete this.report;
+      delete this.result;
     });
   }
 };
 
 // Start our tests
-glob.sync('test-files/error-*.js', {cwd: __dirname}).forEach(function checkErrorFile (_filepath) {
+glob.sync('test-files/error-curly-braces.js', {cwd: __dirname}).forEach(function checkErrorFile (_filepath) {
   var filepath = path.join(__dirname, _filepath);
   describe('An invalid file "' + _filepath + '"', function () {
     describe('when linted', function () {
@@ -37,22 +47,24 @@ glob.sync('test-files/error-*.js', {cwd: __dirname}).forEach(function checkError
 
       it('receives its expected error', function () {
         var expected = require(filepath).expected;
-        expect(this.report.errorCount).to.be.at.least(1);
-        expect(this.report.warningCount).to.equal(0);
+        expect(this.result.errorCount).to.be.at.least(1);
+        expect(this.result.warningCount).to.equal(0);
+        expect(this.messageStr).to.match(expected);
       });
     });
   });
 });
 
+// TODO: Enable skipped tests
 glob.sync('test-files/ignore-*.js', {cwd: __dirname}).forEach(function checkIgnoreFile (_filepath) {
   var filepath = path.join(__dirname, _filepath);
-  describe.only('A valid file "' + _filepath + '"', function () {
+  describe.skip('A valid file "' + _filepath + '"', function () {
     describe('when linted', function () {
       testUtils.lint(filepath);
 
       it('has no errors and no warnings', function () {
-        expect(this.report.errorCount).to.equal(0);
-        expect(this.report.warningCount).to.equal(0);
+        expect(this.result.errorCount).to.equal(0);
+        expect(this.result.warningCount).to.equal(0);
       });
     });
   });
@@ -60,17 +72,17 @@ glob.sync('test-files/ignore-*.js', {cwd: __dirname}).forEach(function checkIgno
 
 glob.sync('test-files/warn-*.js', {cwd: __dirname}).forEach(function checkWarnFile (_filepath) {
   var filepath = path.join(__dirname, _filepath);
-  describe('A warning valid file "' + _filepath + '"', function () {
+  describe.skip('A warning valid file "' + _filepath + '"', function () {
     describe('when linted', function () {
       testUtils.lint(filepath);
 
       it('has no errors', function () {
-        expect(this.report.errorCount).to.equal(0);
+        expect(this.result.errorCount).to.equal(0);
       });
 
       it('receives its expected warning', function () {
         // TODO: Add expected check
-        expect(this.report.warningCount).to.be.at.least(1);
+        expect(this.result.warningCount).to.be.at.least(1);
         expect(false).to.equal(true);
       });
     });
