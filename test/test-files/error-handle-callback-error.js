2e206de181c735de235668dd3630dbd5f3703f5c
Todd Wolfson <todd@twolfson.com>
2017-05-27 23:55:30 -0700

    Added error handling rules

---
+++ test/test-files/error-handle-callback-error.js
@@ -0,0 +1,7 @@
+exports.expected = /Expected error to be handled/i;
+
+// Require errors to be handled
+function main(err, results) {
+  // Code goes here...
+}
+void main;
---
+++ test/test-files/off-ignore-callback-stderr.js
@@ -0,0 +1,5 @@
+// Don't recognize `stderr` as a false positive to handle
+function main(stderr, stdout) {
+  // Code goes here...
+}
+void main;
