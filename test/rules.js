dec10df88c3c7e11840a903f77cdd0c65d28e6f7
Todd Wolfson <todd@twolfson.com>
2017-05-27 21:59:21 -0700

    Iterating on test setup

--- test/rules.js
+++ test/rules.js
@@ -1,95 +1,77 @@
 // Load in dependencies
-var exec = require('child_process').exec;
 var expect = require('chai').expect;
 var glob = require('glob');
 var path = require('path');
-var quote = require('shell-quote').quote;
+var eslint = require('eslint');
+
+// Load in our config
+var eslintConfigFilepath = require.resolve('../');
 
 // Define test utilities
 var testUtils = {
-  exec: function (cmd) {
-    // Run the command and save our results
-    before(function execCmd (done) {
-      var that = this;
-      exec(cmd, function handleExec (err, stdout, stderr) {
-        that.err = that.err || err;
-        that.stdout = (that.stdout || '') + stdout;
-        that.stderr = (that.stdout || '') + stderr;
-        done();
-      });
-    });
-
-    // Clean up results
-    after(function cleanup () {
-      delete this.err;
-      delete this.stdout;
-      delete this.stderr;
-    });
-  },
-  precheck: function (filepath) {
-    // Generate our command
-    var jshintCmd = require.resolve('jshint/bin/jshint');
-    var cmd = quote([
-      'node', jshintCmd, '--config',
-      path.join(__dirname, '/../lib/rc/jshintrc-critical.json'), filepath]);
-    testUtils.exec(cmd);
-  },
   lint: function (filepath) {
-    // Lint our file with both JSCS and JSHint
-    var jshintCmd = require.resolve('jshint/bin/jshint');
-    var jscsCmd = require.resolve('jscs/bin/jscs');
-    testUtils.exec(quote(['node', jshintCmd, filepath]));
-    testUtils.exec(quote(['node', jscsCmd, filepath]));
+    before(function lintFn () {
+      // Define our CLI engine
+      // http://eslint.org/docs/developer-guide/nodejs-api#executeonfiles
+      var cli = new eslint.CLIEngine({
+        configFile: eslintConfigFilepath,
+        useEslintrc: false
+      });
+
+      // Lint our file and save the results
+      var report = cli.executeOnFiles([filepath]);
+      this.report = report;
+    });
+    after(function cleanup () {
+      delete this.report;
+    });
   }
 };
 
 // Start our tests
-glob.sync('node/invalid-*.js', {cwd: __dirname + '/test-files/'}).forEach(function checkInvalidFile (_filepath) {
-  var filepath = path.join(__dirname, 'test-files', _filepath);
+glob.sync('test-files/error-*.js', {cwd: __dirname}).forEach(function checkErrorFile (_filepath) {
+  var filepath = path.join(__dirname, _filepath);
   describe('An invalid file "' + _filepath + '"', function () {
     describe('when linted', function () {
       testUtils.lint(filepath);
 
       it('receives its expected error', function () {
         var expected = require(filepath).expected;
-        expect(this.err).to.not.equal(null);
-        expect(this.stdout).to.match(expected);
+        expect(this.report.errorCount).to.be.at.least(1);
+        expect(this.report.warningCount).to.equal(0);
       });
     });
   });
 });
 
-glob.sync('node/valid-*.js', {cwd: __dirname + '/test-files/'}).forEach(function checkValidFile (_filepath) {
-  var filepath = path.join(__dirname, 'test-files', _filepath);
-  describe('A valid file "' + _filepath + '"', function () {
+glob.sync('test-files/ignore-*.js', {cwd: __dirname}).forEach(function checkIgnoreFile (_filepath) {
+  var filepath = path.join(__dirname, _filepath);
+  describe.only('A valid file "' + _filepath + '"', function () {
     describe('when linted', function () {
       testUtils.lint(filepath);
 
-      it('has no errors', function () {
-        expect(this.err).to.equal(null);
+      it('has no errors and no warnings', function () {
+        expect(this.report.errorCount).to.equal(0);
+        expect(this.report.warningCount).to.equal(0);
       });
     });
   });
 });
 
-glob.sync('node/precheck-*.js', {cwd: __dirname + '/test-files/'}).forEach(function checkInvalidFile (_filepath) {
-  var filepath = path.join(__dirname, 'test-files', _filepath);
-  describe('A precheck valid file "' + _filepath + '"', function () {
-    describe('when prechecked', function () {
-      testUtils.precheck(filepath);
-
-      it('has no errors', function () {
-        expect(this.err).to.equal(null);
-      });
-    });
-
+glob.sync('test-files/warn-*.js', {cwd: __dirname}).forEach(function checkWarnFile (_filepath) {
+  var filepath = path.join(__dirname, _filepath);
+  describe('A warning valid file "' + _filepath + '"', function () {
     describe('when linted', function () {
       testUtils.lint(filepath);
 
-      it('receives its expected error', function () {
-        var expected = require(filepath).expected;
-        expect(this.err).to.not.equal(null);
-        expect(this.stdout).to.match(expected);
+      it('has no errors', function () {
+        expect(this.report.errorCount).to.equal(0);
+      });
+
+      it('receives its expected warning', function () {
+        // TODO: Add expected check
+        expect(this.report.warningCount).to.be.at.least(1);
+        expect(false).to.equal(true);
       });
     });
   });
